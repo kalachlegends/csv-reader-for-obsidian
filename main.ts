acad8d9feda64261985e90bd6eabe1754b4b1a47
Artem Kalachev <funduk101@yandex.ru>
2023-05-01 20:40:24 +0600

    feat: report

--- main.ts
+++ main.ts
@@ -1,89 +1,143 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	Vault,
+} from "obsidian";
+import { getAPI } from "obsidian-dataview";
 // Remember to rename these classes and interfaces!
-
+import { parse } from "csv-parse";
 interface MyPluginSettings {
 	mySetting: string;
+	year: number;
+	month: number;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+	year: 2023,
+	month: 5,
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
-
+		const api = getAPI();
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		// 	// Called when the user clicks the icon.
+		// 	new Notice('This is a notice!');
+		// });
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Status Bar Text");
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "open-sample-modal-simple",
+			name: "Open sample modal (simple)",
+			callback: async () => {
+				console.log(
+					api,
+					api.index.etags.delegate.invMap.get(this.settings.mySetting)
+				);
+				const array = api.index.etags.delegate.invMap.get(
+					this.settings.mySetting
+				);
+				let object = {
+					time_sleep: 0,
+					english: 0,
+					work: 0,
+					score: 0,
+					algoritms: 0,
+					focus_time: 0,
+					sport: 0
+				}
+				array.forEach((e) => {
+					const page = this.app.plugins.plugins.dataview.api.index.pages.get(e)
+						console.log(page.ctime.c.year , parseInt(this.settings.year) , page.ctime.c.month)
+					if(page.ctime.c.year == parseInt(this.settings.year) && page.ctime.c.month == parseInt(this.settings.month)) {
+
+						const regeXp = /\+/g
+				
+						console.log(page.fields.get)
+						if(typeof  page.fields.get("score") == "number"){
+							object.score = object.score + page.fields.get("score")
+						}
+						if(typeof page.fields.get("focus_time") == "number"){
+							object.focus_time = object.focus_time + page.fields.get("focus_time")
+						}
+
+						if(typeof page.fields.get("time_sleep") == "number"){
+							object.time_sleep = object.time_sleep + page.fields.get("time_sleep")
+						}
+						console.log(page.fields.get("algoritms"))
+						if( page.fields.get("algoritms")){
+							const find = page.fields.get("algoritms").match(regeXp)
+							if (find)
+								object.algoritms = object.algoritms + find.length
+						}
+
+						if( page.fields.get("english")){
+							console.log(page.fields.get("english"))
+							const find = page.fields.get("english").match(regeXp)
+							if (find)
+								object.english = object.english + find.length
+						}
+
+						if( page.fields.get("work")){
+							const find = page.fields.get("work").match(regeXp)
+							if (find)
+								object.work = object.work + find.length
+						}
+						console.log(page.fields.get("sport"))
+						if(typeof page.fields.get("sport") == "string"){
+							const find = page.fields.get("sport").match(regeXp)
+							if (find)
+								object.sport = object.sport + find.length
+						}
+					}
+					});
+
+				console.log(object)
+				// console.log(this.app.plugins.plugins.dataview.api.index.pages("#diary/daily"))
+			},
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,21 +145,21 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// class SampleModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+// 	onOpen() {
+// 		const {contentEl} = this;
+// 		contentEl.setText('Woah!');
+// 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+// 	onClose() {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
@@ -116,22 +170,52 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Tag report")
+			.setDesc("Tag report")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						console.log("Secret: " + value);
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Year Report")
+			.setDesc("Year Report")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.year)
+					.onChange(async (value) => {
+						console.log("Secret: " + value);
+						this.plugin.settings.year = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Year Report")
+			.setDesc("Mounth Report")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.month)
+					.onChange(async (value) => {
+						console.log("Secret: " + value);
+						this.plugin.settings.month = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
